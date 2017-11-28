17effc7c92ae20c1453575607460b564c87d7a6a
Brian Bokser <brian.bokser@gmail.com>
2017-11-28 10:56:39 -0300

    EscaleVisitor con unica instancia

--- src/AST_visitors.py
+++ src/AST_visitors.py
@@ -15,31 +15,37 @@ class EscaleVisitor(Visitor):
         expr.e = self.e
 
     def visitDiv(self, expr):
-        expr.leftExpr.accept(copy(self))
-        expr.rightExpr.accept(copy(self))
+        expr.leftExpr.accept(self)
+        expr.rightExpr.accept(self)
 
     def visitConcat(self, expr):
-        expr.leftExpression.accept(copy(self))
-        expr.rightExpression.accept(copy(self))
+        expr.leftExpression.accept(self)
+        expr.rightExpression.accept(self)
 
     def visitSubSuper(self, expr):
-        expr.mainExpr.accept(copy(self))
-        expr.superExpr.accept(EscaleVisitor(0.7*self.e))
-        expr.subExpr.accept(EscaleVisitor(0.7*self.e))
+        e = self.e
+        expr.mainExpr.accept(self)
+        self.e = e * 0.7
+        expr.superExpr.accept(self)
+        expr.subExpr.accept(self)
+        self.e = e
 
     def visitSuperSub(self, expr):
-        expr.mainExpr.accept(copy(self))
-        expr.superExpr.accept(EscaleVisitor(0.7*self.e))
-        expr.subExpr.accept(EscaleVisitor(0.7*self.e))
+        e = self.e
+        expr.mainExpr.accept(self)
+        self.e = e * 0.7
+        expr.superExpr.accept(self)
+        expr.subExpr.accept(self)
+        self.e = e
 
     def visitSuperSuffix(self, supersuffix_expr):
-        supersuffix_expr.expr.accept(copy(self))
+        supersuffix_expr.expr.accept(self)
 
     def visitSubSuffix(self, subsuffix_expr):
-        subsuffix_expr.expr.accept(copy(self))
+        subsuffix_expr.expr.accept(self)
 
     def visitGroupedPar(self, grouped_expr):
-        grouped_expr.expr.accept(copy(self))
+        grouped_expr.expr.accept(self)
 
 class WidthVisitor(Visitor):
 
